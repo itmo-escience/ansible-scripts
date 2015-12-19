f490f0ea066be7b0551e538431a919a26c22cad8
fonhorst <alipoov.nb@gmail.com>
2015-12-19 22:47:52 +0400

    minor changes to use the project as a submodule

--- install-sshkey.sh
+++ install-sshkey.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+PATH_TO_PROJECT=$1
+
 if [ ! -f ~/.ssh/id_rsa_vagrant.pub ]; then
-    cp /home/vagrant/project/sshkey/id_rsa.pub /home/vagrant/.ssh/id_rsa_vagrant.pub;
-    cat /home/vagrant/project/sshkey/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+    cp $PATH_TO_PROJECT/sshkey/id_rsa.pub /home/vagrant/.ssh/id_rsa_vagrant.pub;
+    cat $PATH_TO_PROJECT/sshkey/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
     echo 'The key from sshkey has been succesfully installed'
 else
     echo 'The key from sshkey is already installed'
