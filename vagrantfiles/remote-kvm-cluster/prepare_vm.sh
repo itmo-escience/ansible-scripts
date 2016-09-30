22664086d3a1e93ae22a5a8608a6dd0d7741c73c
fonhorst <alipoov.nb@gmail.com>
2016-09-30 16:43:54 +0400

    vm preparation to run yahoo_benchmarks

--- vagrantfiles/remote-kvm-cluster/prepare_vm.sh
+++ vagrantfiles/remote-kvm-cluster/prepare_vm.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+
+set -e
+
+# install proxy
+echo 'export http_proxy=http://proxy.ifmo.ru:3128' >> ~/.bashrc
+echo 'export HTTP_PROXY=http://proxy.ifmo.ru:3128' >> ~/.bashrc
+
+# get git
+sudo apt-get install -y git
+
+# get maven
+wget http://apache-mirror.rbc.ru/pub/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+tar -xzf apache-maven-3.3.9-bin.tar.gz
+echo 'export MVN=/home/vagrant/apache-maven-3.3.9/bin/mvn' >> ~/.bashrc
+
+# get lein
+wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+chmod a+x lein
+echo 'export LEIN=/home/vagrant/lein' >> ~/.bashrc
