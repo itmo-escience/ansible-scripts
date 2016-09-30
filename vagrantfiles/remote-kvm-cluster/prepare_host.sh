b0c34f10526b57f46f28d9d334b36fc3ed17d9e8
smirnp <nano@node13n.b14.lan>
2016-09-30 16:39:59 +0300

    fixed host preparation

--- vagrantfiles/remote-kvm-cluster/prepare_host.sh
+++ vagrantfiles/remote-kvm-cluster/prepare_host.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+## run it in the way: ssh -tt 192.168.13.112 'bash -s' < prepare_host.sh
 set -e
 
 sudo yum install -y libxslt-devel libxml2-devel libvirt-devel libguestfs-tools-c ruby-devel
@@ -14,5 +15,3 @@ echo "</pool>" >> pool.xml
 sudo virsh pool-create pool.xml
 
 sudo service libvirtd restart
-
-vagrant plugin install vagrant-libvirt
