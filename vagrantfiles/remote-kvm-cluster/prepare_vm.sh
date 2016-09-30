6dc13390e165e16a342e39dea9fc35a5bf955ed5
fonhorst <alipoov.nb@gmail.com>
2016-09-30 17:01:44 +0400

    fixed preparation of vms to install yahoo_\benchmarks

--- vagrantfiles/remote-kvm-cluster/prepare_vm.sh
+++ vagrantfiles/remote-kvm-cluster/prepare_vm.sh
@@ -7,7 +7,10 @@ echo 'export http_proxy=http://proxy.ifmo.ru:3128' >> ~/.bashrc
 echo 'export HTTP_PROXY=http://proxy.ifmo.ru:3128' >> ~/.bashrc
 
 # get git
-sudo apt-get install -y git
+sudo http_proxy=http://proxy.ifmo.ru:3128 HTTP_PROXY=http://proxy.ifmo.ru:3128 apt-get install -y git
+
+export http_proxy=http://proxy.ifmo.ru:3128 
+export HTTP_PROXY=http://proxy.ifmo.ru:3128
 
 # get maven
 wget http://apache-mirror.rbc.ru/pub/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
